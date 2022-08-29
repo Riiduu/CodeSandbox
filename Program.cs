f35bd91f908cb25cdf8428fd2c4e6c30bf0cf437
Riiduu <kiwijelinsk@gmail.com>
2022-08-30 00:28:57 +0300

    pis tav mamm

--- Program.cs
+++ Program.cs
@@ -5,11 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("manmamm");
-<<<<<<< Updated upstream
-
-=======
             Console.WriteLine("pis tavmamm");
->>>>>>> Stashed changes
         }
     }
 }
